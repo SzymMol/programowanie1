eba30d5d1373b8a8f0887c97635d4d288ebeec23
SzymMol <74541025+SzymMol@users.noreply.github.com>
2020-12-02 09:04:01 +0100

    Create kostka.cpp

---
+++ kostka.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+    random_device r;
+    default_random_engine e(r());
+    uniform_int_distribution<int> uniform_dist (1,6);
+    return uniform_dist(e);
+}
